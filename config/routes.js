d3e385c283a58a273c42dfa239607909c9a42f07
Trio Nugroho <ntrio23331@gmail.com>
2022-11-18 16:30:58 +0700

    add new router

--- config/routes.js
+++ config/routes.js
@@ -16,7 +16,11 @@ const apiRouter = express.Router();
  *       implementations
  */
  apiRouter.use(cors())
-apiRouter.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+apiRouter.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+apiRouter.use('/', (req, res) => {
+  res.send('Hallo!!!')
+})
 
 apiRouter.post("/api/v1/google", controllers.api.v1.handleGoogleLoginOrRegister);
 
